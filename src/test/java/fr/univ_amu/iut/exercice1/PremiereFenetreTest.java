8352a0c7e90e586b5a037d3b3b5606cd722d73a9
BANTI-Hugo <hugobanti1007@gmail.com>
2026-04-27 12:02:01 +0000

    feat: exercice1

--- src/test/java/fr/univ_amu/iut/exercice1/PremiereFenetreTest.java
+++ src/test/java/fr/univ_amu/iut/exercice1/PremiereFenetreTest.java
@@ -3,7 +3,6 @@ package fr.univ_amu.iut.exercice1;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -20,9 +19,9 @@ class PremiereFenetreTest {
   void start(Stage stage) throws Exception {
     this.stage = stage;
     new PremiereFenetre().start(stage);
+    this.stage.show();
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void laFenetreEstVisible(FxRobot robot) {
     assertThat(stage.isShowing()).as("le Stage doit être affiché après un appel à show()").isTrue();
